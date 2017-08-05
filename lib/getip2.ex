83d9db96c0b650c7a75032c8b6245459cd889f61
Christopher Blanchard <cablanchard@gmail.com>
2017-08-05 22:37:34 +0100

    Initial commit

---
+++ lib/getip2.ex
@@ -0,0 +1,18 @@
+defmodule Getip2 do
+  @moduledoc """
+  Documentation for Getip2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Getip2.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/getip2_test.exs
@@ -0,0 +1,8 @@
+defmodule Getip2Test do
+  use ExUnit.Case
+  doctest Getip2
+
+  test "greets the world" do
+    assert Getip2.hello() == :world
+  end
+end
